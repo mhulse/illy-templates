7d334a8e21d94ef98e527d06932f44d280a88fa7
Micky Hulse <mickyhulse@gmail.com>
2016-01-23 21:00:49 -0800

    House cleaning.

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Illustrator default template folder:
 INSTALL_DIR="$(cd /Applications/Adobe\ Illustrator*/Cool\ Extras.localized/en_US/Templates/@mhulse; pwd)";
 
 # Remote repo zip file containing all templates:
 SOURCE_ZIP="https://github.com/mhulse/illy-templates/tarball/master";
-#SOURCE_ZIP=./illy-templates-master.zip;
 
 # Clean previous installs:
 rm -rf "$INSTALL_DIR";
@@ -15,7 +14,6 @@ mkdir -pv "$INSTALL_DIR";
 
 # Get the zip file and extract all template files:
 curl -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 -C "$INSTALL_DIR" "*.ait";
-#tar -xzf "$SOURCE_ZIP" --strip-components 1 -C "$INSTALL_DIR" "*.ait";
 
 # Done!
 # For more information about this script, see:
