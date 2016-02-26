1992aa2b0f3099bc2a8d95d7aa9e78b44160c3e5
Micky Hulse <mickyhulse@gmail.com>
2016-02-25 21:11:38 -0800

    Updated comments

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ function install() {
 	# Installation folder:
 	target="@mhulse"
 	
-	# Illustrator default template folder:
+	# Illustrator default folder:
 	INSTALL_DIR="/Applications/$1/Cool Extras.localized/en_US/Templates/"
 	
 	# Navigate to the installation directory:
@@ -24,10 +24,10 @@ function install() {
 	# Create installation folder:
 	mkdir -pv "$target"
 	
-	# Remote repo zip file containing all templates:
+	# Remote repo zip file:
 	SOURCE_ZIP="https://github.com/mhulse/illy-templates/tarball/master"
 	
-	# Get the zip file and extract all template files:
+	# Get the zip file and extract all files:
 	curl -sS -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 -C "./$target" "*.ait";
 	
 	# Let the use know that we are done:
